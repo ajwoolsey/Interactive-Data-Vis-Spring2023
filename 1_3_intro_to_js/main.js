6a313e352b6e11ef4ff734ff492cbafccdc43ddf
ajwoolsey <awoolsey@gradcenter.cuny.edu>
2023-02-13 12:33:14 -0500

    Modifed js file to show button with click count

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,41 @@
 console.log('hello world');
+
+console. log(document)
+console. log(window)
+
+const input= document.getElementById("name-input")
+console.log(input)
+
+console
+
+const updateName= () => {
+    console. log('in update function')
+    const userName= input.value;
+    const myString = `hey my name is`+ userName
+    window.alert(`hello, welcome to class ${userName}`)
+}
+    
+
+updateName();
+
+let changeable = true;
+const constant = true
+
+function change () {
+    changeable = false;
+    const constant= false;
+    console.log(changeable, constant)
+}
+
+change();
+console.log(changeable, constant)
+
+const array= ("apple", "orange", "banana", "mango", "toast")
+
+const newArray= array.localeCompare((c) => {
+    console.log(`c`,c)
+    return `my favorite food is ${c}`
+})
+
+console.log(newArray)
+
