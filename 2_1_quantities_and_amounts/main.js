3bfd420e94c7c74536c3a1237901a81aa504a9ac
ajwoolsey <awoolsey@gradcenter.cuny.edu>
2023-02-22 14:43:36 -0500

    Modified 1_2

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -3,16 +3,40 @@
 // const width = ;
 // const height = ;
 
-/* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
-  .then(data => {
-    console.log("data", data)
+const data = [
+  { day: 'Sunday', crashes: 13664},
+  { day: 'Monday', crashes: 17279},
+  { day: 'Tuesday', crashes: 17337},
+  { day: 'Wednesday', crashes:  17394},
+  { day: 'Thursday', crashes: 17394},
+  { day: 'Friday', crashes: 19147},
+  { day: 'Saturday', crashes: 15714}
+];
 
-    /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
-    
+//create svg to hold bar chart
+<svg width= "500" height= "400"></svg>
 
-    /* HTML ELEMENTS */
-    /** Select your container and append the visual elements to it */
+//creating svg with preselected container div. selecter for div id is #
+const svg= d3.select('#container')
+  margin = 200,
+  width = svg.attr("width") - margin,
+  height = svg.attr("height") - margin
 
-  })
+//Scale
+  const x= d3.ScaleBand ()
+    .domain(d3.range(data, length))
+    .range(margin.left, width - margin.right)
+    .padding(0.1);
+  
+  const y= d3.ScaleLinear()
+    .domain([10000, 20000])
+    .range([height - margin.bottom, margin.top]);
+
+//ready to add rectangles
+svg
+  .append('g')
+  .attr('fill', 'purple')
+  .selectAll('rect')
+  .data(data.sort((a, b) => d3.descending(a.score, b,score)))
+  .join('rect')
+   
